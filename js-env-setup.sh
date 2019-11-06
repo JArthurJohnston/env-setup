d9cf9966644241f05720f712f7259d39daaf0354
Joe Johnston <joe.a.johnston@gmail.com>
2019-11-06 09:17:27 -0500

    added js setup file

---
+++ js-env-setup.sh
@@ -0,0 +1,10 @@
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+npm i -g eslint
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension oshri6688.javascript-test-runner
+code --install-extension esbenp.prettier-vscode
+
+

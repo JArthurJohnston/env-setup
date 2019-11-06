9a6193005e9dc4ae572e080519b0b9987e13614c
J. Arthur <joe.a.johnston@gmail.com>
2019-11-06 09:44:07 -0500

    Update js-env-setup.sh
    added steps to increase the vs code file watch limit

--- js-env-setup.sh
+++ js-env-setup.sh
@@ -8,3 +8,8 @@ code --install-extension oshri6688.javascript-test-runner
 code --install-extension esbenp.prettier-vscode
 
 
+sudo su
+
+echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
+
+sysctl -p
